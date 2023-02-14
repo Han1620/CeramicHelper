3884f33abe76a99e69da3718f86f8ae9c6f93ed6
Hannah Adnan Abdulrehman <hannahabdulrehman@gmail.com>
2023-02-14 16:10:17 +0000

    initial pieces of code for constructor objects

---
+++ js/newTask.js
@@ -0,0 +1,144 @@
+console.log("hello.");
+
+function Task(name, steps) {
+  this.name = name;
+  this.steps = steps;
+  Task.allTasks.push(this);
+}
+
+Task.allTasks = [];
+
+const strCylinder = [
+  {
+    desc: "Press thumb into the center of the clay. Check the thickness of the base using a dry pin tool.",
+    completed: false,
+  },
+  {
+    desc: "Pull outwards, from the center towards yourself.",
+    completed: false,
+  },
+  {
+    desc: "Compress walls, using a sponge with pressure on your edges- do this after every step going forward.",
+    completed: false,
+  },
+  {
+    desc: "Compress the base of your pot using pressing pulling outwards with a sponge.",
+    completed: false,
+  },
+  {
+    desc: "Using both hands, push walls inwards at an angle.",
+    completed: false,
+  },
+  {
+    desc: "Using your thumb, remove clay from the outward base, to create a ridge.",
+    completed: false,
+  },
+  {
+    desc: "Using both hands, pull the clay up, startin gfrom the ridge. Do this in a smoth motion, with light/medium pressure.",
+    completed: false,
+  },
+  {
+    desc: "Reapeat pulling the clay up, and compressing the walls, until you reach your desired height/wall thickness.",
+    completed: false,
+  },
+  {
+    desc: "Using a rib, hold the flat edge,with light pressure, against the outside of your vessel. Simultaneously, have your other hand inside the pot counteracting the pressure.",
+    completed: false,
+  },
+];
+
+const firstTask = new Task("Throwing a straight-edged cylinder", strCylinder);
+console.log(firstTask);
+
+const basicTurning = [
+  {
+    desc: "Throw your piece with a thick base- the requirements will be determined by how deep you want your turning to be.",
+    completed: false,
+  },
+  {
+    desc: "Leave to dry overnight in a damp room, base-down on the wooden board.",
+    completed: false,
+  },
+  {
+    desc: "Once it's leatherhard, remove from the board, and place rim-down on the wheel.",
+    completed: false,
+  },
+  {
+    desc: "Center your piece.",
+    completed: false,
+  },
+  {
+    desc: "Secure your piece in place using clay balls.",
+    completed: false,
+  },
+  {
+    desc: "Cut into your base using a needle tool, starting from the center and gradually pulling outwards, creating a spiral.This will break up any uneven surface. You may need to do this twice. Pressure: light. Wheel speed: fast.",
+    completed: false,
+  },
+  {
+    desc: "Using an flat-edged hoop tool, do the same motion. This will remove clay, and flatten the base. Keep a steady hand.",
+    completed: false,
+  },
+  {
+    desc: "Now you make it individual. Mark out all the parameters of your deign beforehand with a pencil or needle tool.",
+    completed: false,
+  },
+  {
+    desc: "Use the same needle-tool-spiral-technique to cut away large pieces of clay, this will relieve pressure from your piece.",
+    completed: false,
+  },
+  {
+    desc: "Personalise your form using whatever turning tools you have at your disposal, to create diffeerent shapes to the foot of your piece. Consistently bare in mind the thickness of your base, and don't make it too thin.",
+    completed: false,
+  },
+  {
+    desc: "Once done, leave your piece to dry gradually in a damp room rim-down on a wooden board",
+    completed: false,
+  },
+  {
+    desc: "When your piece is bone dry, it is ready to bisk fire.",
+    completed: false,
+  },
+];
+
+const secondTask = new Task("Turning the base of your piece", basicTurning);
+console.log(secondTask);
+
+function renderTask() {
+  console.log("help");
+  const taskElem = document.getElementById("tasks");
+
+  for (i = 0; i < Task.allTasks.length; i++) {
+    const h3Elem = document.createElement("h3");
+    h3Elem.textContent = Task.allTasks[i].name;
+    taskElem.appendChild(h3Elem);
+
+    const list = document.createElement("ul");
+    taskElem.appendChild(list);
+    // another for loop, that loops through Task.allTasks[i].steps and makes an li for each item in the array, and appends it to the ul you created before
+    for (i = 0; i < Task.allTasks.length; i++) {
+      const listItem = document.createElement("li");
+      listItem.textContent = Task.allTasks[i].steps;
+      list.appendChild(listItem);
+      //   // Task.allTasks[i].steps = document.createElement("li");
+      //   // list.appendChild(li);
+      // }
+    }
+  }
+}
+
+renderTask();
+
+// this was my first attempt:
+//  document.Task.steps.addEventListener('keypress', function(event)) {
+//   for (i = 0; i < Task.allTasks.length; i++) {
+//     if (document.Task.steps[i].event == 'true');
+//     document.Task.steps[i].completed == 'true';
+//   }
+// }
+
+// 1) i want to create a click function, to mark objects within the "steps" array as true/false.
+
+// 2) I want the loop to check all my array objects within "steps" array and if they have been clicked, to change them to true.
+
+// 3) I want to save this to local data if they have marked any as true.
